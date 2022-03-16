598fb047fbe25d2082e48f8dc7443cd2c81b8626
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-16 17:31:40 +0600

    start project reactjs core concepts2

--- src/App.js
+++ src/App.js
@@ -4,22 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
     </div>
   );
+}
+
+function Person() {
+  return (
+    <div className="person">
+      <h1>Name: </h1>
+      <h4>Role: </h4>
+    </div>
+
+  )
 }
 
 export default App;
