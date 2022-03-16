a2a82f360b7de87b98ac130ac542dee7d2ac2d35
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-16 21:08:10 +0600

    dynamic load data from jsonholder

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
 
-function Counter() {
-  const [count, setCount] = useState(0)
-
-  const increaseCount = () => setCount(count + 1)
-  const decreaseCount = () => setCount(count - 1)
-
-  // const increaseCount = () => {
-  //   const newCount = count + 1;
-  //   setCount(newCount);
-  // }
-
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
   return (
     <div>
-      <h1>Count: {count} </h1>
-      <button onClick={increaseCount}>Increase</button>
-      <button onClick={decreaseCount}>Decrease</button>
+      <h1>External User Information</h1>
+      {
+        users.map(user => <User name={user.name} email={user.email} phone={user.phone} website={user.website}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div>
+      <h2>User Name: {props.name}</h2>
+      <h4>User Email: {props.email}</h4>
+      <h4>User Website: {props.website}</h4>
+      <h4>User Phone: {props.phone}</h4>
     </div>
   )
 }
