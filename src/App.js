b1d7897ba21fb64781bc38be672ff3e6b5a66f68
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-16 19:21:38 +0600

    dynamic ui show array of object data

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const instruments = [
+    { name: 'Computer', price: '35000' },
+    { name: 'Mobile', price: '25000' },
+    { name: 'Head Phone', price: '1000' },
+    { name: 'Sound Box', price: '800' }
+  ]
   return (
     <div className="App">
-      <Person></Person>
+      {
+        instruments.map(instrument => <Person name={instrument.name} price={instrument.price}></Person>)
+      }
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
   return (
     <div className="person">
-      <h1>Name: </h1>
-      <h4>Role: </h4>
+      <h1>Name: {props.name}</h1>
+      <h4>Price: {props.price}</h4>
     </div>
 
   )
 }
+
 
 export default App;
